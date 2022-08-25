341013bdfe94e55e1be6edae393c75e0b942fb89
Emiliyan Ognyanov <emilian.e.ognianov@gmail.com>
2022-08-25 17:20:05 +0300

    list comprehensions

--- chapter_02/list_comprehensions.py
+++ chapter_02/list_comprehensions.py
@@ -14,3 +14,13 @@ print(beyond_ascii)
 
 beyond_ascii = list(filter(lambda c: c > 127, map(ord, symbols)))
 print(beyond_ascii)
+
+# Cartesian product
+colors = ['black', 'white']
+
+sizes = ['S', 'M', 'L']
+
+t_shirts = [(color, size) for color in colors for size in sizes]
+
+print(t_shirts)
+
